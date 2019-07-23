856aead6c26e91abacdc3abdb684d9b0905083a7
Piotr Jaczewski <pp.jaczewski@gmail.com>
2019-07-23 08:42:47 +0200

    Updates

---
+++ precache-manifest.a11ba142e02979d6247ae457a1438c94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c40eb79c756164c6e9d7",
+    "url": "/Recipe-Search-App/static/js/runtime~main.2a8de2bd.js"
+  },
+  {
+    "revision": "eb8fc39324715ebb2165",
+    "url": "/Recipe-Search-App/static/js/main.251a2390.chunk.js"
+  },
+  {
+    "revision": "a7611df06a1d524570e1",
+    "url": "/Recipe-Search-App/static/js/2.07a7384f.chunk.js"
+  },
+  {
+    "revision": "eb8fc39324715ebb2165",
+    "url": "/Recipe-Search-App/static/css/main.284b2b7b.chunk.css"
+  },
+  {
+    "revision": "1e46314c110049ef4afe4b45a02b106d",
+    "url": "/Recipe-Search-App/index.html"
+  }
+];
